65cf225137765cc02c4621499548fdc0e81d3958
Rob Vandelinder <robjvan@gmail.com>
2021-08-05 01:07:07 -0230

    Created health widget

---
+++ lib/widgets/battery_health_widget.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class BatteryHealthWidget extends StatelessWidget {
+  BatteryHealthWidget({
+    required this.health,
+    required this.capacity,
+  });
+  final String? health;
+  final int? capacity;
+
+  @override
+  Widget build(BuildContext context) {
+    final double _sw = MediaQuery.of(context).size.width;
+
+    String _healthString = '';
+    Color _healthColor = Colors.green;
+
+    switch (health) {
+      case 'heath_good':
+        _healthString = 'Good';
+        _healthColor = Colors.green;
+        break;
+      case 'heath_bad':
+        _healthString = 'Bad';
+        _healthColor = Colors.red;
+        break;
+    }
+
+    return Container(
+      width: (_sw / 2) - 24,
+      height: (_sw / 2) - 24,
+      child: Column(
+        children: [
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Estimated capacity',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              SizedBox(width: 8),
+              GestureDetector(
+                child: Icon(
+                  Icons.info_outline,
+                  size: 18,
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        contentPadding: const EdgeInsets.all(16),
+                        title: Text(
+                          'Disclaimer',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            child: Text(
+                              'OK',
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .color,
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          )
+                        ],
+                        content: Text(
+                          'This is an estimate and may not be accurate.  Do not use this app as proof for service requests.',
+                          style: TextStyle(fontFamily: 'Montserrat'),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$capacity ',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              Text(
+                'mAh',
+                style: TextStyle(fontFamily: 'Montserrat'),
+              ),
+            ],
+          ),
+          Spacer(),
+          Text(
+            'Overall health',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+            ),
+          ),
+          SizedBox(height: 4),
+          Text(
+            '$_healthString',
+            style: TextStyle(
+              fontSize: 32,
+              color: _healthColor,
+              fontFamily: 'Montserrat',
+            ),
+          ),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
